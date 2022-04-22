8751bfe89f2b97a585ece9f121575c83735cb33c
GuiMunizR <guilherme.muniz@outlook.com.br>
2022-04-22 01:35:25 -0300

    Commit 20220422-0135

--- AZURE-VM-NIC-DISABLE-ACCELERATED_NETWORK.ps1
+++ AZURE-VM-NIC-DISABLE-ACCELERATED_NETWORK.ps1
@@ -1,5 +1,7 @@
-#AZURE-VM-NIC-DISABLE-ACCELERATED_NETWORK
-##OBJETIVE: DISABLE ACCELERATED NETWORKING ON A NIC
+#Created by: Guilherme Muniz (linkedin.com/in/guilhermemunizr)
+##NAME: AZURE-VM-NIC-DISABLE-ACCELERATED_NETWORK
+###OBJETIVE: DISABLE ACCELERATED NETWORKING ON A NIC
+
 #FONT:
     #https://docs.microsoft.com/en-us/cli/azure/service-page/list%20a%20-%20z?view=azure-cli-latest
 
@@ -10,23 +12,23 @@
     # On CMD RUN: az upgrade
 
 #LOGIN TO AZURE BY BROWSER
-az LOGIN
+    az LOGIN
 
 #DECLARE TO THE FOLLOWING VARIABLES
-$SubscriptionName = "<Variable value>"
-$NICName = "<Variable value>"
-$RGName = "<Variable value>"
-$Status = "<False or true>"
+    $SubscriptionName = "<Variable value>"
+    $NICName = "<Variable value>"
+    $RGName = "<Variable value>"
+    $Status = "<False or true>"
 
 #Set a subscription to be the current active subscription.
-az account set --subscription $SubscriptionName
+    az account set --subscription $SubscriptionName
 #Get the details of a subscription.
-az account show
+    az account show
 #List resource groups.
-az group list
+    az group list
 #List resources.
-az resource list
+    az resource list
 #Update a network interface.
-az network nic update --name $NICName --resource-group $RGName --accelerated-networking $Status
+    az network nic update --name $NICName --resource-group $RGName --accelerated-networking $Status
 #Log out to remove access to Azure subscriptions.
-az logout
+    az logout
